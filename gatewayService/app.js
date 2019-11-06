5b1b61d58f4614444bf1b1b90e167a65fc36da1c
Kathryn Lovett <klovett@cbtnuggets.com>
2019-11-05 16:24:53 -0800

    Updating to use Graph Manager

--- gatewayService/app.js
+++ gatewayService/app.js
@@ -1,14 +1,17 @@
 const { ApolloServer } = require('apollo-server');
 const { ApolloGateway } = require('@apollo/gateway');
 
-const gateway = new ApolloGateway({
-    serviceList: [
-        { name: 'user', url: 'http://localhost:4001' },
-        { name: 'review', url: 'http://localhost:4002' }
-    ]
-});
+const config = require('./config');
 
-const server = new ApolloServer({ gateway, subscriptions: false });
+const gateway = new ApolloGateway();
+
+const server = new ApolloServer({
+    gateway,
+    subscriptions: false,
+    engine: {
+        apiKey: config.gatewayServiceKey
+    }
+});
 server.listen().then(({ url }) => {
     console.log(`🚀  Gateway ready at ${url}`);
 });
--- userService/fixtures.js
+++ userService/fixtures.js
@@ -2,16 +2,19 @@ module.exports = {
     1: {
         id: 1,
         name: 'Sarah Connor',
-        favoriteColor: 'Blue'
+        favoriteColor: 'Blue',
+        starSign: 'Leo'
     },
     2: {
         id: 2,
         name: 'Terminator',
-        favoriteColor: 'Red'
+        favoriteColor: 'Red',
+        starSign: 'Scorpio'
     },
     3: {
         id: 3,
         name: 'Kyle Reese',
-        favoriteColor: 'Green'
+        favoriteColor: 'Green',
+        starSign: 'Taurus'
     }
 }
